a24591086566a0515b569d8331694a75a477d89b
Adrian Woong Jekal <lufee84@gmail.com>
2017-07-31 15:58:53 +1000

    updated

--- ourcity/webapp/urls.py
+++ ourcity/webapp/urls.py
@@ -2,4 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')]
+    # /webapp/
+    url(r'^$', views.index, name='index'),
+
+    # /webapp/71/
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+]
