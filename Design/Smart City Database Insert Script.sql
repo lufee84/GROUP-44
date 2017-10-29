9a177308fb2309699f85c254a7e61030674f863c
Adrian Woong Jekal <lufee84@gmail.com>
2017-10-29 23:45:00 +1000

    Updated

---
+++ Design/Smart City Database Insert Script.sql
@@ -0,0 +1,37 @@
+INSERT INTO user VALUES (1, "admin", "admin@smartcity.com", "Password111", "admin");
+INSERT INTO user VALUES (2, "user_1", "user_1@hello.com", "Postman323", "tourist");
+INSERT INTO user VALUES (3, "bighat", "bighat@gmail.com", "LGODL123", "student");
+INSERT INTO user VALUES (4, "roki33", "nash2@gmail.com", "Psowk22", "business");
+INSERT INTO city VALUES (1, "Brisbane", "QLD");
+INSERT INTO city VALUES (2, "Sydney", "NSW");
+INSERT INTO city VALUES (3, "Melbourne", "VIC");
+INSERT INTO college VALUES (1, "QUT Gardens Point Campus", "2 George St, Brisbane City QLD 4000", 
+								"Science And Engineering, Business, Creative Industries", "askqut@qut.edu.au");
+INSERT INTO college VALUES (2, "University of Queensland", "St Lucia Qld 4072", "Science And Engineering, LAW", 
+								"uq.edu.au");
+INSERT INTO hotel VALUES(40, 1, "Brisbane Marriott Hotel", "515 Queen Street, Brisbane QLD 4000", "(07) 3303 8000", 
+							     "brisbane@marriott.com");
+INSERT INTO hotel VALUES(50, 1, "NEXT Hotel", "66 Queen Street Mall, Brisbane QLD 4000", "(07) 3222 3222", 
+							     "info.brisbane@nexthotels.com");                                     
+INSERT INTO industries VALUES(230, 2, "ABC Electrical", "7 Barrinia St, Slacks Creek, QLD 4127", "Electrical Appliances Services & Parts", 
+								"ABCELEl@gmail.com");
+INSERT INTO industries VALUES(120, 3, "Supagas Brisbane", "8 Production St, Beenleigh QLD 4207", "LPG Gas Suppliers", 
+								"supergasbrisbane@gmail.com");                                
+INSERT INTO library VALUES(1, 1, "Annerley Library", "450 Ipswich Road, Annerley", "(07) 3138 2000", "No email");
+INSERT INTO library VALUES(2, 1, "New Farm Library", "135 Sydney St New Farm, 4005", "(07) 3403 1062", "bsql@brisbane.qld.gov.au");                                
+INSERT INTO mall VALUES(342, 1, "Brisbane Arcade", "160 Queen St, Brisbane City QLD 4000", "(07) 3231 9777"
+								"info@bne.mcgees.com.au");
+INSERT INTO mall VALUES(346, 1, "Sunnybank Plaza", "Mains Rd & McCullough St, Sunnybank QLD 4109", "(07) 3231 9777"
+								"sunnybankplaza@retailfirst.com.au");                                
+INSERT INTO museum VALUES(10, 1, "Museum of Brisbane", "192 Ann St, Brisbane City", "(07) 3345 7500", 
+								"info@museumofbrisbane.com.au");                                
+INSERT INTO park VALUES(20, 1, "King Edward Park", "224 Turbot St, Brisbane City QLD 4000", "(07) 3403 8888", "No email");
+INSERT INTO restaurant VALUES(1, 1, "Guilty Rogue Public House", "109 Edward St, Brisbane City", "(07) 3403 8888", 
+								"guilty_ho@gmail.com");
+INSERT INTO restaurant VALUES(3, 1, "Cafe O-Mai", "15 Cracknell Rd, Annerley QLD 4103", "(07) 3255 9778", 
+								"No email");
+INSERT INTO zoo VALUES(1, 1, "Australia Zoo", "1638 Steve Irwin Way, Beerwah", "(07) 5436 2000", 
+							 "info@australiazoo.com.au");
+INSERT INTO zoo VALUES(2, 1, "Lone Pine Koala Sanctuary", "708 Jesmond Rd, Fig Tree Pocket QLD 4069", "(07) 3378 1366", 
+							 "service@koala.net");                             
+                          
